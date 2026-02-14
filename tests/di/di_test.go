01b0b46772b9aa0bc4b776ba5413c9fe1bdd19f8
andet tafa <andet.tafa@verso.de>
2026-02-15 00:06:29 +0100

    =

--- tests/di/di_test.go
+++ tests/di/di_test.go
@@ -2,13 +2,16 @@ package di_test
 
 import (
 	"testing"
+
 	"github.com/a-digi/coco-server/server/di"
+	"github.com/a-digi/coco-logger/logger"
+	db "github.com/a-digi/coco-orm/orm"
 )
 
 type dummyContext struct{}
 
-func (d dummyContext) GetDatabaseManager() interface{} { return nil }
-func (d dummyContext) GetLogger() interface{} { return nil }
+func (d dummyContext) GetDatabaseManager() *db.DatabaseManager { return nil }
+func (d dummyContext) GetLogger() logger.Logger               { return nil }
 
 func TestContextInterface(t *testing.T) {
 	var _ di.Context = dummyContext{}
