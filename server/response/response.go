8b3314ffb2b1f71d95000e76305c0d42478a5dd9
andet tafa <andet.tafa@verso.de>
2026-02-15 19:10:35 +0100

    =

--- server/response/response.go
+++ server/response/response.go
@@ -35,6 +35,8 @@ func NotFoundResponse(w http.ResponseWriter, message string) {
 func buildHeaders(w http.ResponseWriter) {
     w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Max-Age", "0")
 	w.Header().Set("Content-Type", "application/json")
 }
